cd18f6cb3b68e2272cd0095887ae6c5f4f651445
oussama abbas <abbasoussama1@gmail.com>
2022-12-01 11:29:30 +0100

    try envs

--- src/controllers/twitos.controller.js
+++ src/controllers/twitos.controller.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
+
 const test = async (req, res) => {
   try {
-    res.status(200).json({ valueDuHeader: req.params.username });
+    res.status(200).json({
+      valueDuHeader: req.params.username,
+      apikey: process.env.API_KEY,
+      BIGBOSS: process.env.BIGBOSS || 'personne',
+    });
   } catch (error) {
     res.status(500).send('Une erreur est survenue');
   }
